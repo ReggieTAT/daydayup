c9f18bf7a318b4421e99d8693bc02bd0ff650569
ReggieTAT <32627085+ReggieTAT@users.noreply.github.com>
2018-07-16 17:23:20 +0800

    添加AdministerDao缺少的包

--- src/main/java/dao/AdministerDao.java
+++ src/main/java/dao/AdministerDao.java
@@ -7,6 +7,7 @@ import javabean.WaitedCard;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
